ac2f6dc9476d31c5726ea92e7793d6db55aa9783
Matias Hernandez <matiasfh@gmail.com>
2016-04-28 09:42:24 -0300

    get all pull requets from a repository

--- src/plugins/hearForPrCommands.js
+++ src/plugins/hearForPrCommands.js
@@ -6,9 +6,30 @@ const setup = (token) =>
 		auth: 'oauth'
 	})
 
+const getOpenPrs = (user, repo, github) => {
+	const repo = github.getRepo(user, repo)
+	const pulls = repo.listPulls({
+		state: 'open'
+	})
+	console.log(pulls)
+}
+
+const getUsername = (configs) => {
+	//Get username/organization
+	let userName = configs.organization
+	if(configs.hasOwnProperty('user')) {
+		userName = configs.user
+	}
+	return userName
+}
+
 export const hear = (controller, configs) => {
 	controller.on('slash_command', (bot, msg) => {
-		console.log(msg)
+		const regex = /report prs (.*)/
+		const match = msg.text.match(regex)
+		if(match) {
+			getOpenPrs(getUsername(configs), match[1])
+		}
 		bot.replyPrivate(msg, 'Hi')
 	})
 }
